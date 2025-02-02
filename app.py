b47657262a48fc2580e028d2ef823d5a1b2b5933
sambathreasmey <reasmeysambath@gmail.com>
2025-02-02 12:11:52 +0700

    enhance user

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import datetime
 import os
 from flask import Flask, render_template, redirect, request, url_for, session, flash
 from flask_wtf import FlaskForm
-from wtforms import DateField, SelectField, StringField,PasswordField,SubmitField
+from wtforms import BooleanField, DateField, SelectField, StringField,PasswordField,SubmitField
 from wtforms.validators import DataRequired, Email
 from dotenv import load_dotenv
 
@@ -11,27 +11,30 @@ from internal_connector import RestConnector
 #Load environment
 load_dotenv()
 secret_key = os.getenv('app.secret_key')
+channel_id = "sambathreasmey"
 
 app = Flask(__name__)
 app.secret_key = secret_key
 
 class RegisterForm(FlaskForm):
-    user_name = StringField("Name",validators=[DataRequired()])
-    email_address = StringField("Email",validators=[DataRequired(), Email()])
-    password = PasswordField("Password",validators=[DataRequired()])
-    submit = SubmitField("Create Account")
+    full_name = StringField("ឈ្មោះពេញ",validators=[DataRequired()])
+    user_name = StringField("ឈ្មោះអ្នកប្រើប្រាស់",validators=[DataRequired()])
+    email_address = StringField("អ៊ីមែល",validators=[DataRequired(), Email()])
+    password = PasswordField("ពាក្យសម្ងាត់",validators=[DataRequired()])
+    submit = SubmitField("បង្កើតគណនី")
 
 class SavingDepositForm(FlaskForm):
     amount = StringField("ចំនួនទឹកប្រាក់", validators=[DataRequired()])
     currencyType = SelectField('ប្រភេទសាច់ប្រាក់', choices=[('USD', 'USD'), ('USD', 'KHR')])
     date = DateField('កាលបរិច្ឆេទ', format='%Y-%m-%d', default=datetime.date.today, validators=[DataRequired()])
     desc = StringField("កំណត់ចំណាំ")
+    isMoreDeposit = BooleanField("បន្ថែមលើប្រតិបត្តិចាស់ក្នុងថ្ងៃ")
     submit = SubmitField("បញ្ចូល")
 
 class LoginForm(FlaskForm):
-    username = StringField("Username",validators=[DataRequired()])
-    password = PasswordField("Password",validators=[DataRequired()])
-    submit = SubmitField("Login")
+    username = StringField("ឈ្មោះអ្នកប្រើប្រាស់",validators=[DataRequired()])
+    password = PasswordField("ពាក្យសម្ងាត់",validators=[DataRequired()])
+    submit = SubmitField("ចូលគណនី")
 
 @app.route('/')
 def index():
@@ -39,7 +42,7 @@ def index():
         user_id = session['user_id']
 
         req = {
-            "channel_id": "sambathreasmey"
+            "channel_id": channel_id
         }
         res = RestConnector.internal_app_api('partner', 'retrive_user', req, "POST")
         if res:
@@ -53,7 +56,7 @@ def index():
                             user_detail = user
                 
                     req = {
-                        "channel_id": "sambathreasmey"
+                        "channel_id": channel_id
                     }
                     res = RestConnector.internal_app_api('saving', 'transaction_detail', req, "POST")
                     if res.status_code == 200:
@@ -74,14 +77,15 @@ def saving_deport():
             currencyType = form.currencyType.data
             date = form.date.data
             desc = form.desc.data
+            isMoreDeposit = form.isMoreDeposit.data
             req = {
                 "transaction_date": date.strftime('%Y-%m-%d'),
                 "amount": amount,
                 "transaction_desc": desc,
                 "user_id": session['user_id'],
                 "currency_type": currencyType,
-                "transaction_type": "normal",
-                "channel_id": "sambathreasmey"
+                "transaction_type": "saving_deposit_more" if isMoreDeposit else "saving_deposit",
+                "channel_id": channel_id
             }
             resp = RestConnector.internal_app_api('saving', 'deposit', req, "POST")
             if resp:
@@ -102,7 +106,7 @@ def delete_transaction_by_id(transaction_id):
     if 'user_id' in session:
         req = {
             "transaction_id": transaction_id,
-            "channel_id": "sambathreasmey"
+            "channel_id": channel_id
         }
         resp = RestConnector.internal_app_api('saving', 'delete_transaction_by_id', req, "DELETE")
         if resp:
@@ -121,8 +125,8 @@ def update_transaction_by_id(transaction_id):
             "amount": request.json.get("amount"),
             "transaction_desc": request.json.get("transaction_desc"),
             "currency_type": request.json.get("currency_type"),
-            "transaction_type": "normal",
-            "channel_id": "sambathreasmey"
+            "transaction_type": request.json.get("transaction_type"),
+            "channel_id": channel_id
         }
         resp = RestConnector.internal_app_api('saving', 'update_transaction_by_id', req, "PUT")
         if resp:
@@ -136,7 +140,7 @@ def update_transaction_by_id(transaction_id):
 def report():
     if 'user_id' in session:
         req = {
-                "channel_id": "sambathreasmey"
+                "channel_id": channel_id
             }
         res = RestConnector.internal_app_api('saving', 'transaction_detail', req, "POST")
         if res:
@@ -153,11 +157,13 @@ def report():
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
+        full_name = form.full_name.data
         user_name = form.user_name.data
         email_address = form.email_address.data
         password = form.password.data
         req = {
-        "channel_id": "sambathreasmey",
+        "channel_id": channel_id,
+        "full_name": full_name,
         "user_name": user_name,
         "password": password,
         "email_address": email_address,
@@ -186,7 +192,7 @@ def login():
         username = form.username.data
         password = form.password.data
         req = {
-            "channel_id": "sambathreasmey",
+            "channel_id": channel_id,
             "user_name": username,
             "password": password
         }
@@ -196,14 +202,23 @@ def login():
                 data = res.json()
                 if data and data['status'] == 0:
                     session['user_id'] = username
+                    session['user_detail'] = data['data']
                     return redirect(url_for('index'))
                 else: 
-                    flash("Login failed. Please check your username and password")
+                    flash("ការចូលបានបរាជ័យ។ សូមពិនិត្យមើលឈ្មោះអ្នកប្រើប្រាស់ និងពាក្យសម្ងាត់របស់អ្នក។")
                     return redirect(url_for('login'))
         flash("ប្រព័ន្ធមានបញ្ហារអាក់រអួល សូមព្យាយាមពេលក្រោយ")
         return redirect(url_for('login'))
 
     return render_template('login.html',form=form)
+
+@app.route('/onload')
+def onload():
+    req = {
+            "user_detail": session['user_detail'],
+            "user_name": session['user_id']
+        }
+    return req
 
 @app.route('/logout')
 def logout():
